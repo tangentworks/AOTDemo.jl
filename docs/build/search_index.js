4c8e710b5bc7a7d6ac2e35e98dd0a05ef54c4a1c
Marek Kukan <marek.kukan@tangent.works>
2018-08-08 02:41:22 +0200

    add docs/build

---
+++ docs/build/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "AOTDemo package",
+    "title": "AOTDemo package",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#AOTDemo-package-1",
+    "page": "AOTDemo package",
+    "title": "AOTDemo package",
+    "category": "section",
+    "text": "This package demonstrates how to build a shared library with C/C++ API from Julia source code."
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "AOTDemo package",
+    "title": "Installation",
+    "category": "section",
+    "text": "julia> Pkg.clone(\"todo\")"
+},
+
+{
+    "location": "index.html#Usage-1",
+    "page": "AOTDemo package",
+    "title": "Usage",
+    "category": "section",
+    "text": "$ julia --color=yes compile.jl"
+},
+
+]}
